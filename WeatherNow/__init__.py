492025a5cc3259af3d89bd5229ab4c4e9b4b0bc2
rseby99 <rseby99@yahoo.com>
2021-12-07 23:58:11 +0200

    venv rebuild

--- WeatherNow/__init__.py
+++ WeatherNow/__init__.py
@@ -1,9 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b867cf8704988f0363c0a4b92ae45bef'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-from WeatherNow import routes
+from WeatherNow import routespip
