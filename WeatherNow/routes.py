62691db28e77c5a4b529a0279209028bf5541960
rseby99 <rseby99@yahoo.com>
2022-01-19 03:10:18 +0200

    added documentation

--- WeatherNow/routes.py
+++ WeatherNow/routes.py
@@ -83,6 +83,8 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
